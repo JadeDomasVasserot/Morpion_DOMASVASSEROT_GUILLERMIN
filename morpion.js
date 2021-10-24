9fc902f5a96d34f4d3749c7b1f2dbc9a725b8203
Jade DOMAS-VASSEROT <75765424+JadeDomasVasserot@users.noreply.github.com>
2021-10-24 21:01:09 +0100

    Morpion Base

---
+++ morpion.js
@@ -0,0 +1,19 @@
+// exemple de variables que vous pouvez utiliser
+var grille = new Array(); // stock la grille avec des cases jouées
+var grilleElement = new Array(); // stock la grille avec les éléments HTML
+var grilleRestant = new Array(); // stock la grille des coups restant à jouer
+var grilleCaseId = new Array(); // stock la grille des id de chaque case
+var ligne = 3;
+var colonne = 3;
+var joueurHumain = 1;
+var joueurIA = 2;
+var imgHumain = "o.gif";
+var imgIA = "x.gif";
+var nbCase = ligne * colonne;
+var nbCoup = ligne * colonne;
+var joueHumain = false;
+
+function Init()
+{
+
+}
